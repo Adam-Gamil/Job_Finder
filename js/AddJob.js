841c65c091d36b2c919db9f55a1594bb11afc3be
Amir abd el Wahed <162839071+amirsloker2005@users.noreply.github.com>
2025-04-25 20:51:39 +0300

    Add files via upload

---
+++ js/AddJob.js
@@ -0,0 +1,43 @@
+import {
+    AllJobs,
+    session,
+    Admin,
+    saveJobsToLocalStorage,
+    saveUsersToLocalStorage,
+    loadSessionFromLocalStorage,
+    Job,
+    Ids,
+} from "./main.js";
+
+
+
+document.getElementById("addJobBtn").addEventListener("click", function () {
+
+    if(session.currUser && session.currUser instanceof Admin){
+    const company = session.currUser.company;
+    const jobTitle = document.getElementById("job-title").value;
+    const jobSalary = document.getElementById("job-salary").value;
+    const experience = document.getElementById("years_exp").value;
+    const statusInput = document.querySelector('input[name="status"]:checked');
+    const status = statusInput ? statusInput.value : null;
+    const requirements = document.getElementById("requirements").value;
+    const description = document.getElementById("description").value;
+    const location = document.getElementById("location").value;
+    Ids.currId++;
+    console.log("Company Name:", company);
+    console.log("Job Title:", jobTitle);
+    console.log("Job Salary:", jobSalary);
+    console.log("Experience:", experience);
+    console.log("Status:", status);
+    console.log("Requirements:", requirements);
+    console.log("Description:", description);
+    console.log("Location:", location);
+    
+    const addedjob = new Job(Ids.currId, company, jobTitle, experience, jobSalary, requirements, location, description, status);
+    session.currUser.addJob(AllJobs, addedjob);
+    console.log(AllJobs);
+    saveJobsToLocalStorage();
+    
+    window.location.href = "viewCreatedJobs.html";
+    }
+});
